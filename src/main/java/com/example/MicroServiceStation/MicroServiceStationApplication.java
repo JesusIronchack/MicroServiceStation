ce1e50c4c756af01f8ef7f964fdfa00189a49e08
JesusCiber <fjesuscl@gmail.com>
2025-05-22 16:28:44 +0200

    init commit

---
+++ src/main/java/com/example/MicroServiceStation/MicroServiceStationApplication.java
@@ -0,0 +1,13 @@
+package com.example.MicroServiceStation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroServiceStationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroServiceStationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/MicroServiceStation/MicroServiceStationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MicroServiceStation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroServiceStationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
